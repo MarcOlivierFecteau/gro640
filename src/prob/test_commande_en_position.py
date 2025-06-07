1c276efb1aa5d70a927b33e3b76c8f45981a3a27
MarcOlivierFecteau <marc-olivier.fecteau@usherbrooke.ca>
2025-06-07 16:41:51 -0400

    Add CLI option for target position and reformat
    The option `--target` enables the user to specify the target coordinates
    of the robot. This enables faster testing for tuning the "magic numbers".

--- src/prob/test_commande_en_position.py
+++ src/prob/test_commande_en_position.py
@@ -1,29 +1,51 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import argparse
 import numpy as np
 
 from gro640_robots import LaserRobot
 from dosg0801_fecm0701 import CustomPositionController  # Empty template
 
 
-# Model cinématique du robot
-sys = LaserRobot()
+def main(args=None):
+    parser = argparse.ArgumentParser(
+        description="Test position controller for LaserRobot."
+    )
+    parser.add_argument(
+        "--target",
+        type=float,
+        nargs=2,
+        default=[0.0, -0.5],
+        help="Target position for the end-effector (2 values)",
+    )
+    parsed_args = parser.parse_args(args)
 
-# Contrôleur en position de l'effecteur standard
-ctl = CustomPositionController(sys)
+    target = np.array(parsed_args.target)
+    if np.shape(target) != (2,):
+        raise ValueError("Target position MUST be a 2 x 1 matrix.")
 
-# Cible de position pour l'effecteur
-ctl.rbar = np.array([0, -1])
+    # Model cinématique du robot
+    sys = LaserRobot()
 
-# Dynamique en boucle fermée
-clsys = ctl + sys
+    # Contrôleur en position de l'effecteur standard
+    ctl = CustomPositionController(sys)
 
-# Configurations de départs
-clsys.x0 = np.array([0, 0.5, 0])  # crash
-# clsys.x0 =  np.array([0,0.7,0]) # fonctionne
+    # Cible de position pour l'effecteur
+    ctl.rbar = target
 
-# Simulation
-clsys.compute_trajectory()
-clsys.plot_trajectory("xu")
-clsys.animate_simulation()
+    # Dynamique en boucle fermée
+    clsys = ctl + sys
+
+    # Configurations de départs
+    clsys.x0 = np.array([0, 0.5, 0])  # crash
+    # clsys.x0 = np.array([0, 0.7, 0])  # fonctionne
+
+    # Simulation
+    clsys.compute_trajectory()
+    clsys.plot_trajectory("xu")
+    clsys.animate_simulation()
+
+
+if __name__ == "__main__":
+    main()
